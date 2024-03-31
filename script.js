64c980f4edc0700b9e1098692d454f29d708679f
Ronit Suman <ronitsuman59@gmail.com>
2024-03-31 15:59:06 +0530

    xz

---
+++ script.js
@@ -0,0 +1,99 @@
+let x = 5;
+let y = 2;
+let z = x + y;
+console.log(z
+    )
+
+    let a = 5;
+let b = 2;
+let c = x * y;
+console.log(c)
+
+
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+
+console.log(result)
+
+let text4 = "amit";
+let text5 = "kumar";
+let text0 = text4 + " " + text5;
+
+console.log(text0)
+
+let texta = "What a very ";
+texta += "nice day";
+
+console.log(texta)
+
+let p = 5;
+let q = 2;
+let r = p % q;
+console.log(r)
+
+let m = 5;
+m++;
+let n = m;
+console.log(n)
+
+let g = 5;
+g--;
+let h = g;
+
+console.log(h)
+
+function myAddition(s,t){
+    console.log(s + t) ;
+}
+let results = myAddition(3,3)
+console.log(myAddition);
+
+function newMultiply(m,n){
+    console.log(m*n);
+}
+let answer = newMultiply(2,2)
+newMultiply(2,5)
+myAddition(5,3)
+
+function newDivide (f,g){
+    console.log(f / g );
+    
+}
+
+newDivide(10,2)
+
+function mathPower(a){
+    console.log(a*a);
+}
+mathPower(50)
+
+function newModulus(s,n){
+    console.log( s % n );
+}
+newModulus(10,3)
+
+function newTask(z,v,c) {
+    console.log(z * v + c);
+}
+newTask(2,2,1)
+
+function newstory(n,b,o){
+    console.log("this " +n * o +"after multiplying of" + n + o + b  );
+}
+newstory(2,3,5)
+
+myAddition(2,2)
+
+newDivide(20,2)
+
+myAddition(27,3)  
+newDivide(9,3)
+
+function myAdd(a,d){
+    console.log(a + d) ;
+}
+let reesult = myAdd(46 , 67)
+console.log(myAdd);
+
+myAdd(40,60)
